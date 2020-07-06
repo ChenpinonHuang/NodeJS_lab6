517d396fe6ff850c9a09635fc0b03e5cb113030a
Chengbin Huang <hcb12586@163.com>
2020-07-05 23:36:39 -0400

    first try

---
+++ controllers/SessionsController.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+const passport = require('passport');
+const viewPath = 'sessions';
+
+// Step 1: Add the login logic
+
+// Step 2: Add the authentication logic
+
+// Step 3: Add the logout logic
+
+exports.new = (req, res) => {
+    res.render(`${viewPath}/new`, {
+      pageTitle: 'Login'
+    });
+  };
+  
+  exports.create = (req, res, next) => {
+    passport.authenticate('local', {
+      successRedirect: '/blogs',
+      successFlash: 'You were successfully logged in.',
+      failureRedirect: '/login',
+      failureFlash: 'Invalid credentials'
+    })(req, res, next);
+  };
+  
+  exports.delete = (req, res) => {
+    req.logout();
+    req.flash('success', 'You were logged out successfully.');
+    res.redirect('/');
+  };
